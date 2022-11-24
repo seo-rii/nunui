481f27ba40081cb348de3faa5918bb9bff1a1326
Seohyun Lee <studioseorii@gmail.com>
2022-11-25 01:42:49 +0900

    refactor: allow import from root

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,39 @@
-// Reexport your entry components here
+import Blocker from "$lib/Blocker";
+import Button from "$lib/Button";
+import Card from "$lib/Card";
+import Checkbox from "$lib/Checkbox";
+import CircularProgress from "$lib/CircularProgress";
+import Hoverable from "$lib/Hoverable";
+import Icon from "$lib/Icon";
+import IconButton from "$lib/IconButton";
+import Input from "$lib/Input";
+import LinearProgress from "$lib/LinearProgress";
+import List from "$lib/List";
+import Paper from "$lib/Paper";
+import Radio from "$lib/Radio";
+import Ripple from "$lib/Ripple";
+import SegmentButton from "$lib/SegmentButton";
+import Table from "$lib/Table";
+import ThemeProvider from "$lib/ThemeProvider";
+import Tooltip from "$lib/Tooltip";
+
+export {
+    Blocker,
+    Button,
+    Card,
+    Checkbox,
+    CircularProgress,
+    Hoverable,
+    Icon,
+    IconButton,
+    Input,
+    LinearProgress,
+    List,
+    Paper,
+    Radio,
+    Ripple,
+    SegmentButton,
+    Table,
+    ThemeProvider,
+    Tooltip
+}
